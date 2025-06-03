678e67b59cb83b71b2f3010f4a02a2f54547ac7a
tmazitov <timurmazitov000@gmail.com>
2025-06-03 13:54:08 +0400

    add 'add friend' button on the profile page

--- src/components/content/profile-page-content/friend-list/FrienListComponent.ts
+++ src/components/content/profile-page-content/friend-list/FrienListComponent.ts
@@ -2,6 +2,7 @@ import FrontendyComponent from "../../../../pkg/frontendy/component/component";
 import { elem } from "../../../../pkg/frontendy/vdom/constructor";
 import Store from "../../../../store/store";
 import User from "../../../../types/User";
+import ButtonComponent from "../../../inputs/ButtonComponent";
 import DeleteFriendModal from "../../../modals/DeleteFriendModal";
 import FriendListItemComponent from "./FriendListItemComponent";
 
@@ -25,7 +26,7 @@ export default class FriendListComponent extends FrontendyComponent {
             
             // Friend list
             elem('div')
-            .setProps({class : "flex flex-col gap-2"})
+            .setProps({class : "flex flex-col gap-2 max-h-96 overflow-auto "})
             .setChild(this.state.friends.map((user:User) => {
 
                 let imagePath: string | null = null;
@@ -45,6 +46,15 @@ export default class FriendListComponent extends FrontendyComponent {
 
                 return new FriendListItemComponent(user)
                     .onDelete(() => this.state.friendToDelete = user)
+            })),
+
+            elem("div")
+            .setProps({class: "mt-4"})
+            .addChild(new ButtonComponent({
+                label: "Add Friend",
+                icon: "ti ti-plus",
+                color: "blue",
+                type: "outline",
             })),
 
 
