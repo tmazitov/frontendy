8b7cdccdfd8da688027926180601d58723b932a3
tmazitov <timurmazitov000@gmail.com>
2025-06-18 12:21:00 +0400

    add contenxt words to the friend invite

--- src/components/content/profile-page-content/friend-list/InvitesListComponent.ts
+++ src/components/content/profile-page-content/friend-list/InvitesListComponent.ts
@@ -31,12 +31,20 @@ export default class InvitesListComponent extends FrontendyComponent {
         Store.setters.deleteFriendInvite(inviteId)
     }
 
+    private nicknameWrapper(nickname: string, isAuthor: boolean) {
+        if (isAuthor) {
+            return `You have send an invite to <span class="text-black text-bold">${nickname}</span>`;
+        }
+        return `<span class="text-black text-bold">${nickname}</span> has sent you an invite`;
+    }
+
     template() {
 
         const elems = this.state.invites.map((invite: FriendInvite) => {
 
             const buttons:Array<FrontendyComponent> = []
-            if (invite.authorId == this.state.userId) {
+            const isAuthor = invite.authorId == this.state.userId
+            if (isAuthor) {
                 buttons.push(new ButtonComponent({
                     icon: "ti ti-trash",
                     type: "outline",
@@ -57,11 +65,25 @@ export default class InvitesListComponent extends FrontendyComponent {
                     size: "small",
                 }))
             }
+            const content = []
+            if (isAuthor) {
+                content.push(...[
+                    elem("span").addChild("You have sent a friend invite to "),
+                    elem("span").setProps({class: "text-black font-medium"}).addChild(invite.nickname)
+                ])
+            } else {
+                content.push(...[
+                    elem("span").setProps({class: "text-black font-medium"}).addChild(invite.nickname),
+                    elem("span").addChild(" has sent you a friend invite")
+                ])
+            }
+
             return elem("div")
             .setProps({class: "friend-item w-full select-none flex items-center justify-between gap-4 py-2 px-4 border-1 border-gray-200 rounded-md hover:border-blue-300 transition-all duration-200"})
             .setChild([
                 elem("div")
-                .addChild(invite.nickname),
+                .setProps({class: "text-gray-700 font-light"})
+                .setChild(content),
 
                 elem("div")
                 .setProps({class: "friend-item-buttons flex items-center gap-4 opacity-0 transition-all duration-200"})
