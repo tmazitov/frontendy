cf2493fc7a0ea1383bffae889dd6f982c1645bf8
tmazitov <timurmazitov000@gmail.com>
2025-06-24 16:15:03 +0400

    add

--- src/data/games.ts
+++ src/data/games.ts
@@ -3,7 +3,7 @@ import Game from "../types/Game";
 const games:Array<Game> = [
     new Game(0, "Training", "Fight against a bot to train your skills.", 1, "ti ti-robot"),
     new Game(1, "Duel", "Face off against another player in a 1v1 duel.", 2, "ti ti-users"),
-    new Game(2, "Turnament", "Become the target of everyone and prove you're the king of the hill.", 4, "ti ti-tournament"),
+    new Game(2, "Tournament", "Become the target of everyone and prove you're the king of the hill.", 4, "ti ti-tournament"),
 ]
 
 export default games;
